99144c2dbc07d51a59ee1b9fc25e2f92752bbe4b
Sawan Kumar <Sawan.Kumar@rsystems.com>
2017-03-29 13:48:44 +0530

    git

--- wp-content/themes/twentyseventeen/template-prominent.php
+++ wp-content/themes/twentyseventeen/template-prominent.php
@@ -11,3 +11,487 @@ $url = get_bloginfo( 'url' );
 $ref = $_SERVER['HTTP_REFERER'] ;
 
 ?>
+<html>
+<head>
+<title>Elite Tour Agra</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/custom.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<!-- <div id="google_translate_element"></div> -->
+    <script type="text/javascript">
+    function googleTranslateElementInit() {
+            new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+        }
+    </script>
+    <script type="text/javascript" src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+	
+	<div class="container-fluid">
+		<div class="row clearfix">
+			<div class="col-sm-3 rightheight pull-right" style="padding: 0;">
+				<div class="servicebg-right">
+					<div class="bg">
+						<a href="index.html"><img src="images/cross.png" ></a>
+						<ul class="customtab-list" style="margin-top: 20px;">
+							<li class="active"><a href="prominent.html">Prominent Guests</a></li>
+							<li style="margin-top: 50px;"><a href="services.html">Services</a></li>
+							<li><a href="aboutme.html">About</a></li>
+							<li><a href="travel.html">Travel Tidbits</a></li>
+							<li><a href="reviews_inner.html">Review</a></li>
+							<li><a href="contact.html">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-9 pull-left">
+				<div class="custom-tab leftheight">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="tour-heading">
+								<h2>Prominent Guests</h2>
+							</div>
+							<div class="tour-banner">
+								<img src="images/inner-banners/guest_final.jpg">
+							</div>
+						</div>
+					</div>
+					<div class="row" style="padding-bottom:50px;"> 
+						<div class="col-sm-4">
+							<div class="prominent-box">
+								<div class="prominent-image first">
+									<div class="prominent-inner">
+										<img src="images/prominent/image001.png">
+									</div>
+								</div>
+								<div class="prominent-text">
+									<h4 class="blue">With Mark Zuckerberg</h4>
+								</div>
+							</div>
+						</div>	
+						<div class="col-sm-4">
+							<div class="prominent-box ">
+								<div class="prominent-image second">
+									<div class="prominent-inner">
+										<img src="images/prominent/image002.png">
+									</div>
+								</div>
+								<div class="prominent-text">
+									<h4 class="red">With Sharon Stone</h4>
+								</div>
+							</div>
+						</div>	
+						<div class="col-sm-4">
+							<div class="prominent-box ">
+								<div class="prominent-image third">
+									<div class="prominent-inner">
+										<img src="images/prominent/image003.png">
+									</div>
+								</div>
+								<div class="prominent-text">
+									<h4 class="yellow">With Prince & Princess of England</h4>
+								</div>
+							</div>
+						</div>	
+						<div class="col-sm-4">
+							<div class="prominent-box ">
+								<div class="prominent-image first">
+									<div class="prominent-inner">
+										<img src="images/prominent/image004.png">
+									</div>
+								</div>
+								<div class="prominent-text">
+									<h4 class="blue">With Leonardo Dicaprio</h4>
+								</div>
+							</div>
+						</div>	
+						<div class="col-sm-4">
+							<div class="prominent-box ">
+								<div class="prominent-image second">
+									<div class="prominent-inner">
+										<img src="images/prominent/image005.png">
+									</div>
+								</div>
+								<div class="prominent-text">
+									<h4 class="red">Reviews from Trip Advisor</h4>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="prominent-box ">
+								<div class="prominent-image third">
+									<div class="prominent-inner">
+										<img src="images/prominent/image006.png">
+									</div>
+								</div>
+								<div class="prominent-text">
+									<h4 class="yellow">Reviews from Trip Advisor</h4>
+								</div>
+							</div>
+						</div>	
+					</div>
+				</div>
+
+				<footer class="footer scroll-here" id="footer">
+					<div class="row footborder">
+						<div class="col-sm-2">
+							<div class="footer-logo">
+								<img src="images/index/logo.png">
+							</div>
+						</div>
+						<div class="col-sm-8">
+							<div class="footer-text">
+								<h3>Contact</h3>
+								<p>It is a long established fact that a reader </p>
+								<ul class="contact-details">
+									<li><a href="#">booking@elitetouragra.com</a></li>
+									<li><span><img src="images/dot.png"></span>(+05) 320 3057970</li>
+									<li><span><img src="images/dot.png"></span><a href="#">Skype  Elite Tour Agra</a></li>
+								</ul>
+
+								<ul class="social-icon">
+									<li><div><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></div></li>
+									<li><div><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></div></li>
+									<li><div><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></div></li>
+									<li><div><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></div></li>
+								</ul>
+								<h6>&#9400; all right reserved</h6>
+							</div>
+						</div>
+						<div class="col-sm-2">
+							<div class="ft-paypal">
+								<a target="_blank" href="https://www.paypal.com/in/home"><img src="images/paypal-payment.png"></a>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="international-logo">
+								<a href="#"><img src="images/International.jpg"></a>
+							</div>
+						</div>
+						<div class="col-sm-2 center">
+							<a class="tripadvisor" style="border-bottom: none !important;" href="https://www.tripadvisor.in/Attraction_Review-g297683-d5779939-Reviews-Elite_Solutions-Agra_Uttar_Pradesh.html" target="_blank">
+							<img class="alignnone size-full wp-image-1393" src="http://elitesolutionsagra.com/wp-content/uploads/2016/09/Untitled-2.jpg" alt="untitled-2" width="150" height="100">
+							</a>
+						</div>
+						<div class="col-sm-7" align="center">
+							<img src="images/tourism-logo-all.png" width="100%">
+						</div>
+					</div>			
+				</footer>
+			</div>
+		</div>
+	</div>
+
+
+<div class="enqbutton">
+	<img src="images/enquarybutton.png">
+</div>
+<div class="tourbutton">
+	<img src="images/tourbutton.png">
+</div>
+<section class="form-section">
+	<div class="form-outer">
+		<div class="form-inner">
+			<div class="form-close text-right">
+				<a href="#">Close <img src="images/formclose.png"></a>
+			</div>
+			<form action="" method="" style="margin-bottom: 0;">
+				<div class="form-list">
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Name:</p>
+							    <input type="text" class="form-control" id="name" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Phone no:</p>
+							    <input type="text" class="form-control" id="number" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Email:</p>
+							    <input type="email" class="form-control" id="email" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Country:</p>
+							    <input type="text" class="form-control" id="query" required>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Arrival Date:</p>
+							    <input type="text" class="form-control" id="name" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Departure Date:</p>
+							    <input type="text" class="form-control" id="number" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Number of Adults:</p>
+							    <input type="email" class="form-control" id="email" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Number of Kids:</p>
+							    <input type="text" class="form-control" id="query" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Travel Type:</p>
+							    <select name="" class="form-control">
+							    	<option value="">Select Travel Type</option>
+							    	<option value="">Family Vacation</option>
+							    	<option value="">Honeymoon</option>
+							    	<option value="">Business</option>
+							    </select>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Hotel Type:</p>
+							    <select name="" class="form-control">
+							    <option value="">Select Hotel</option>
+							    	<option value="">Budget Hotel</option>
+							    	<option value="">Three Star Hotel</option>
+							    	<option value="">Four Star Hotel</option>
+							    	<option value="">Five Star Hotel</option>
+							    	<option value="">Heritage Hotel</option>
+							    	</select>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="form-group">
+							    <p>Message:</p>
+							    <textarea name="message" class="form-control messaget"></textarea>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="form-group text-center">
+								<button type="submit" class="btn btn-default">Submit</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
+
+<!-- ----------2nd-form------------ -->
+<section class="form-section2">
+	<div class="form-outer">
+		<div class="form-inner">
+			<div class="form-close text-right">
+				<a href="#">Close <img src="images/formclose.png"></a>
+			</div>
+			<form action="" method="" style="margin-bottom: 0;">
+				<div class="form-list">
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Name:</p>
+							    <input type="text" class="form-control" id="name" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Phone no:</p>
+							    <input type="text" class="form-control" id="number" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Email:</p>
+							    <input type="email" class="form-control" id="email" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Country:</p>
+							    <input type="text" class="form-control" id="query" required>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Date:</p>
+							    <input type="text" class="form-control" id="name" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>No of Pax:</p>
+							    <input type="text" class="form-control" id="number" required>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>Language in which guide required:</p>
+							    <!-- <input type="email" class="form-control" id="email" required> -->
+							    <select data-placeholder="Choose a Language..." class="form-control">
+									<option value="">Choose a Language...</option>
+									<option value="AF">Afrikanns</option>
+									<option value="SQ">Albanian</option>
+									<option value="AR">Arabic</option>
+									<option value="HY">Armenian</option>
+									<option value="EU">Basque</option>
+									<option value="BN">Bengali</option>
+									<option value="BG">Bulgarian</option>
+									  <option value="CA">Catalan</option>
+									  <option value="KM">Cambodian</option>
+									  <option value="ZH">Chinese (Mandarin)</option>
+									  <option value="HR">Croation</option>
+									  <option value="CS">Czech</option>
+									  <option value="DA">Danish</option>
+									  <option value="NL">Dutch</option>
+									  <option value="EN">English</option>
+									  <option value="ET">Estonian</option>
+									  <option value="FJ">Fiji</option>
+									  <option value="FI">Finnish</option>
+									  <option value="FR">French</option>
+									  <option value="KA">Georgian</option>
+									  <option value="DE">German</option>
+									  <option value="EL">Greek</option>
+									  <option value="GU">Gujarati</option>
+									  <option value="HE">Hebrew</option>
+									  <option value="HI">Hindi</option>
+									  <option value="HU">Hungarian</option>
+									  <option value="IS">Icelandic</option>
+									  <option value="ID">Indonesian</option>
+									  <option value="GA">Irish</option>
+									  <option value="IT">Italian</option>
+									  <option value="JA">Japanese</option>
+									  <option value="JW">Javanese</option>
+									  <option value="KO">Korean</option>
+									  <option value="LA">Latin</option>
+									  <option value="LV">Latvian</option>
+									  <option value="LT">Lithuanian</option>
+									  <option value="MK">Macedonian</option>
+									  <option value="MS">Malay</option>
+									  <option value="ML">Malayalam</option>
+									  <option value="MT">Maltese</option>
+									  <option value="MI">Maori</option>
+									  <option value="MR">Marathi</option>
+									  <option value="MN">Mongolian</option>
+									  <option value="NE">Nepali</option>
+									  <option value="NO">Norwegian</option>
+									  <option value="FA">Persian</option>
+									  <option value="PL">Polish</option>
+									  <option value="PT">Portuguese</option>
+									  <option value="PA">Punjabi</option>
+									  <option value="QU">Quechua</option>
+									  <option value="RO">Romanian</option>
+									  <option value="RU">Russian</option>
+									  <option value="SM">Samoan</option>
+									  <option value="SR">Serbian</option>
+									  <option value="SK">Slovak</option>
+									  <option value="SL">Slovenian</option>
+									  <option value="ES">Spanish</option>
+									  <option value="SW">Swahili</option>
+									  <option value="SV">Swedish </option>
+									  <option value="TA">Tamil</option>
+									  <option value="TT">Tatar</option>
+									  <option value="TE">Telugu</option>
+									  <option value="TH">Thai</option>
+									  <option value="BO">Tibetan</option>
+									  <option value="TO">Tonga</option>
+									  <option value="TR">Turkish</option>
+									  <option value="UK">Ukranian</option>
+									  <option value="UR">Urdu</option>
+									  <option value="UZ">Uzbek</option>
+									  <option value="VI">Vietnamese</option>
+									  <option value="CY">Welsh</option>
+									  <option value="XH">Xhosa</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="form-group">
+							    <p>City in which guide required:</p>
+							    <select class="form-control states" name="state" id="sel12 stateId">
+								   <option value="">Select State</option>
+								  <option value="1">Andaman and Nicobar Islands</option>
+								  <option value="2">Andhra Pradesh</option>
+								  <option value="3">Arunachal Pradesh</option>
+								  <option value="4">Assam</option>
+								  <option value="5">Bihar</option>
+								  <option value="6">Chandigarh</option>
+								  <option value="7">Chhattisgarh</option>
+								  <option value="8">Dadra and Nagar Haveli</option
+								  ><option value="9">Daman and Diu</option>
+								  <option value="10">Delhi</option>
+								  <option value="11">Goa</option>
+								  <option value="12">Gujarat</option>
+								  <option value="13">Haryana</option>
+								  <option value="14">Himachal Pradesh</option>
+								  <option value="15">Jammu and Kashmir</option>
+								  <option value="16">Jharkhand</option>
+								  <option value="17">Karnataka</option>
+								  <option value="18">Kerala</option>
+								  <option value="19">Lakshadweep</option>
+								  <option value="20">Madhya Pradesh</option>
+								  <option value="21">Maharashtra</option>
+								  <option value="22">Manipur</option>
+								  <option value="23">Meghalaya</option>
+								  <option value="24">Mizoram</option>
+								  <option value="25">Nagaland</option>
+								  <option value="26">Odisha</option>
+								  <option value="27">Pondicherry</option>
+								  <option value="28">Punjab</option>
+								  <option value="29">Rajasthan</option>
+								  <option value="30">Sikkim</option>
+								  <option value="31">Tamil Nadu</option>
+								  <option value="32">Telangana</option>
+								  <option value="33">Tripura</option>
+								  <option value="34">Uttar Pradesh</option>
+								  <option value="35">Uttarakhand</option>
+								  <option value="36">West Bengal</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="form-group">
+							    <p>Message:</p>
+							    <textarea name="message" class="form-control messaget"></textarea>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="form-group text-center">
+								<button type="submit" class="btn btn-default">Submit</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
+
+</body>
+</html>
